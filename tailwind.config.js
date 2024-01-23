688f2ffafa7c6a19c851b72ce911a73c46ae22e0
TomG <tomgegs@outlook.com>
2024-01-23 23:10:51 +1000

    STYLE: HeroSEction + VideoOverlay

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
             },
         },
         extend: {
+            fontFamily: {
+                Alexandria: ['Alexandria', 'sans-serif'],
+                Caveat: ['Caveat', 'cursive'],
+            },
             colors: {
                 border: 'hsl(var(--border))',
                 input: 'hsl(var(--input))',
