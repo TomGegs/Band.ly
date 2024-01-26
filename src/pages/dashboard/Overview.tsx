99cf684883a8c0ba3b828a892993bf0a8d1baead
TomG <tomgegs@outlook.com>
2024-01-26 14:00:42 +1000

    FIX: Changed name from Band.ly to Band.IT

--- src/pages/dashboard/Overview.tsx
+++ src/pages/dashboard/Overview.tsx
@@ -5,19 +5,19 @@ const Overview = () => {
         <div className="w-full mt-5">
             {' '}
             <Grid numItems={1} numItemsSm={2} numItemsLg={4} className="gap-2">
-                <Card>
+                <Card decoration="right" decorationColor='teal'>
                     <Text>Total Revenue</Text>
                     <Metric>$96,990</Metric>
                 </Card>
-                <Card>
+                <Card decoration="right" decorationColor='rose'>
                     <Text>Cost</Text>
                     <Metric>$68,220</Metric>
                 </Card>
-                <Card>
+                <Card decoration="right" decorationColor='violet'>
                     <Text>Active Bands</Text>
                     <Metric>12</Metric>
                 </Card>
-                <Card>
+                <Card decoration="right" decorationColor='green'>
                     <Text>Margin</Text>
                     <Metric>+$28,770</Metric>
                 </Card>
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
             <div className="mx-auto flex h-full w-full flex-col justify-center gap-y-4 rounded-r-2xl bg-black p-12 text-center text-white">
                 <HeroSection />
             </div>
-            <div className="mx-auto flex h-full w-full flex-col justify-center gap-y-4 rounded-r-2xl bg-black text-center text-white">
+            <div className="mx-auto flex h-full w-full flex-col justify-center gap-y-4 rounded-r-2xl bg-black pr-12 text-center text-white">
                 <LoginForm />
             </div>
         </div>
